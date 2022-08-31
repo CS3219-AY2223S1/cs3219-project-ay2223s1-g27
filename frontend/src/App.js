6773ced9af10691d9c834eb61b9aa128bb3ef5af
yongxiangng <yongxiangng@gmail.com>
2022-09-01 00:38:17 +0800

    Add testing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
+import TestingPage from './components/TestingPage';
 import {Box} from "@mui/material";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/test" element={<TestingPage/>}/>
                     </Routes>
                 </Router>
             </Box>
---
+++ frontend/src/components/TestingPage.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import axios from "axios";
+
+function TestingPage() {
+  const URI_USER_SVC = process.env.URI_USER_SVC || "http://localhost:8000";
+  const URI_MATCHING_SVC = process.env.URI_MATCHING_SVC || "http://localhost:8001";
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+
+  axios.get(URI_USER_SVC).then((res) => setA(res.data));
+  axios.get(URI_MATCHING_SVC).then((res) => setB(res.data));
+  return (
+    <>
+      <p>{a}</p>
+      <p>{b}</p>
+    </>
+  );
+}
+
+export default TestingPage;
