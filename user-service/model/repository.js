9e4ff1fdc12ee7abf3bc783c78156c7c31ebd6cd
Ng Zhen Teng <ngzhenteng@gmail.com>
2022-09-01 15:57:40 +0800

    Add a method to check for user existence in the database in repository.js

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -11,7 +11,22 @@ mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-export async function createUser(params) { 
-  return new UserModel(params)
+export async function createUser(params) {
+  let userModelDocument = new UserModel(params)
+  console.log(params)
+  let user = await UserModel.findOne(params).exec()
+  // query.then(function (err, result) {
+  //   if (result == null) {
+  //     console.log("NONE")
+  //   } else {
+  //     console.log(result)
+  //   }
+  // })
+  console.log(user)
+  if (user == null) {
+    return userModelDocument
+  } else {
+    throw "User already exists"
+  }
 }
 
