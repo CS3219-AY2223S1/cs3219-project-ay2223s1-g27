c57c9e9129e12c0064f8d1c4e3b06a1531b3a280
Ng Zhen Teng <ngzhenteng@gmail.com>
2022-09-08 16:47:14 +0800

    Update Create user api to require email, add reset password api

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -7,6 +7,8 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 const jwtAccessSecretKey = process.env.JWT_ACCESS_SECRET
+const emailRegex = RegExp("\\S+@\\S+\\.\\S+")
+
 
 function verifyAuthHeaderFormat(authHeaderSplit) {
     if (authHeaderSplit.length != 2) {
@@ -29,13 +31,16 @@ export function getJWTTokenFromAuthHeader(authHeader) {
 
 export async function createUser(req, res) {
     try {
-        const { username, password } = req.body;
-        if (username && password) {
+        const { username, password, email } = req.body;
+        if (username && password && email) {
+            if (!emailRegex.test(email)) {
+                return res.status(400).json({message: 'Could not create a new user, email format invalid. Email has to contain the @ and . character', success:false});
+            }
             const existing_user = await _getUser(username)
             if (existing_user !== null) {
                 return res.status(400).json({message: 'Could not create a new user, already exists!', success:false});
             }
-            const resp = await _createUser(username, password);
+            const resp = await _createUser(username, password, email);
             if (resp.err) {
                 return res.status(400).json({message: 'Could not create a new user!', success:false});
             } else {
@@ -105,7 +110,6 @@ export async function deleteUser(req, res) {
     }
 }
 
-// Specify {"key":"<value>"} in the request body to update the field "key" for the specified user in mongoDB.
 export async function updatePassword(req, res) {
     try {
         const username = req.body.username
@@ -126,8 +130,41 @@ export async function updatePassword(req, res) {
         if (err instanceof jwt.TokenExpiredError) {
             return res.status(401).json({message: "JWT Token has Expired.", success:false})
         } else {
-            return res.status(400).json({message: "Problem verifying JWT token or updating user details.", success:false})
+            return res.status(400).json({message: "Problem verifying JWT token or updating user password.", success:false})
         }
     }
+}
 
+export async function resetPassword(req, res) {
+    try {
+        const username = req.body.username
+        const newPassword = req.body.newPassword
+        const email = req.body.email
+        if (username && email && newPassword) {
+            if (!emailRegex.test(email)) {
+                return res.status(400).json({message: 'Could not reset password, email format invalid. Email has to contain the @ and . character', success:false});
+            }
+            const existingUser = await _getUser(username)
+            var userEmail
+            if (existingUser) {
+                userEmail = existingUser.email
+                if (!userEmail) {
+                    return res.status(500).json({message: "No email address for specified user", success:false})
+                }
+                if (userEmail != email) {
+                    return res.status(400).json({message: "Email address does not match database records", success:false})
+                }
+            }
+            const updatedUser = await _updatePassword(username, newPassword)
+            if (updatedUser === null) {
+                return res.status(500).json({message: "Error updating user in database", success:false})
+            } else {
+                return res.status(200).json({username: updatedUser.username, message: `Resetted user ${updatedUser.username} password successfully!`, success:true})
+            }
+        } else {
+            return res.status(400).json({message: "'Email address, username and/or new password are missing!", success:false})
+        }
+    } catch (err) {
+        return res.status(400).json({message: "Problem resetting user password.", success:false})
+    }
 }
