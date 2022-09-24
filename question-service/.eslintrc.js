f2e2cf3c65a4acf74e60816a70129058705b1494
ngchisern <ncsern2000@gmail.com>
2022-09-24 10:45:44 +0800

    Add lint and prettier scripts

--- question-service/.eslintrc.js
+++ question-service/.eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
-  }
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
 };
