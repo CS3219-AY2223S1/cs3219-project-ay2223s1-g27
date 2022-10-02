1e23ac76ebff5e319ec3cd60f3335f5311733829
ngchisern <ncsern2000@gmail.com>
2022-10-02 12:25:52 +0800

    Update frontend configs

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,4 +1,10 @@
-const URI_USER_SVC = process.env.REACT_APP_URI_USER_SVC || "http://localhost:8000";
+const LIVE_BACKEND = "http://34.160.41.4"
+const DEV_BACKEND = "http://backend:3001"
+
+const URI_USER_SVC = process.env.ENV = 'DEV' ? DEV_BACKEND : LIVE_BACKEND;
+const URI_MATCHING_SVC = process.env.ENV = 'DEV' ? DEV_BACKEND : LIVE_BACKEND;
+const URI_QUESTION_SVC = process.env.ENV = 'DEV' ? DEV_BACKEND : LIVE_BACKEND;
+const URI_COLLAB_SVC = process.env.ENV = 'DEV' ? DEV_BACKEND : LIVE_BACKEND;
 
 const PREFIX_USER_SVC = "/api/user";
 const PREFIX_CREATEUSER = "/createuser";
@@ -14,20 +20,17 @@ export const URL_USER_SVC_RESETPASSWORD = URL_USER_SVC + PREFIX_RESETPASSWORD;
 export const URL_USER_SVC_LOGOUT = URL_USER_SVC + PREFIX_LOGOUT;
 export const URL_USER_SVC_REFRESH_TOKEN = URL_USER_SVC + PREFIX_REFRESH_TOKEN;
 
-const URI_MATCHING_SVC = process.env.REACT_APP_URI_MATCHING_SVC || "http://localhost:8001";
 export const PREFIX_MATCHING_SVC = "/api/matching";
 export const URL_MATCHING_SVC = URI_MATCHING_SVC;
 
 const PREFIX_QUESTION = "/question";
 const PREFIX_QUESTIONS = "/questions";
 
-const URI_QUESTION_SVC = process.env.REACT_APP_URI_QUESTION_SVC || "http://localhost:8002";
 export const PREFIX_QUESTION_SVC = "/api/question";
 export const URL_QUESTION_SVC = URI_QUESTION_SVC + PREFIX_QUESTION_SVC;
 export const URL_QUESTION_SVC_QUESTION = URL_QUESTION_SVC + PREFIX_QUESTION;
 export const URL_QUESTION_SVC_QUESTIONS = URL_QUESTION_SVC + PREFIX_QUESTIONS;
 
-const URI_COLLAB_SVC = process.env.REACT_APP_URI_COLLAB_SVC || "http://localhost:8003";
 export const PREFIX_COLLAB_SVC = "/api/collab";
 export const URL_COLLAB_SVC = URI_COLLAB_SVC;
 
