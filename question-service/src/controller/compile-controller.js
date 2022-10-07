b99a5ac7a2ed8dc1e5d686aede5280d7eaccf518
ngchisern <ncsern2000@gmail.com>
2022-10-07 13:42:48 +0800

    Add compile controller

---
+++ question-service/src/controller/compile-controller.js
@@ -0,0 +1,23 @@
+const judge = require('../rapid_api/judge.js')
+
+async function compile(req, res) {
+  try {
+    var formData = req.body;
+
+    var resp = await judge.compile(formData);
+
+    if (resp.error) {
+      console.log(`Get Leetcode question error: titleSlug=${titleSlug}, err=${resp.error}`);
+      return res.status(500).json({ message: resp.error, success: false });
+    }
+
+    return res.status(200).json(resp);
+  } catch (err) {
+    console.log(`compile api error: ${err}`);
+    res.status(500).json({ success: false });
+  }
+}
+
+module.exports = {
+  compile
+}
