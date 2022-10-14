ba832eeca758d52be315bb729f449ac6a4432afb
ngchisern <ncsern2000@gmail.com>
2022-10-15 01:12:39 +0800

    Handle code updating in the matching service

--- collab-service/controller/collab-controller.js
+++ collab-service/controller/collab-controller.js
@@ -1,39 +1,46 @@
+var map = {};
+
 export function registerHandlers(io, socket) {
-    console.log('a user connected');
- 
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
 
-    socket.on('room', function(data) {
-        console.log(`a user joined room=${data.room_id}`)
-        socket.join(data.room_id);
-    });
+  console.log('a user connected');
 
-    socket.on('leave room', (data) => {
-        console.log("received leaving")
-        socket.broadcast.to(data.room_id).emit('receive leave', data)
-        socket.leave(data.room_id)
-    });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
-    socket.on('coding event', function(data) {
-        console.log(`received coding ${data.room_id}`)
-        socket.broadcast.to(data.room_id).emit('receive code', data)
-    });
+  socket.on('room', function(data) {
+    console.log(`a user joined room=${data.room_id}`)
+    socket.join(data.room_id);
+    setInterval(() => {
+      if (map[data.room_id])
+        socket.broadcast.to(data.room_id).emit('receive code', map[data.room_id]);
+    }, 500)
+  });
 
-    socket.on('language event', function(data) {
-        console.log(`received language ${data.room_id}`)
-        console.log(`langauge id is ${data.language_id}`)
-        socket.broadcast.to(data.room_id).emit('receive language', data)
-    });
+  socket.on('leave room', (data) => {
+    console.log("received leaving")
+    socket.broadcast.to(data.room_id).emit('receive leave', data)
+    socket.leave(data.room_id)
+  });
 
-    socket.on('question event', function(data) {
-        console.log(`received question ${data.room_id}`)
-        socket.broadcast.to(data.room_id).emit('receive question', data)
-    });
+  socket.on('coding event', function(data) {
+    console.log(`received coding ${data.room_id}`)
+    map[data.room_id] = data
+  });
 
-    socket.on('output event', function(data) {
-        console.log(`received output ${data.room_id}`)
-        socket.broadcast.to(data.room_id).emit('receive output', data)
-    });
+  socket.on('language event', function(data) {
+    console.log(`received language ${data.room_id}`)
+    console.log(`langauge id is ${data.language_id}`)
+    socket.broadcast.to(data.room_id).emit('receive language', data)
+  });
+
+  socket.on('question event', function(data) {
+    console.log(`received question ${data.room_id}`)
+    socket.broadcast.to(data.room_id).emit('receive question', data)
+  });
+
+  socket.on('output event', function(data) {
+    console.log(`received output ${data.room_id}`)
+    socket.broadcast.to(data.room_id).emit('receive output', data)
+  });
 }
