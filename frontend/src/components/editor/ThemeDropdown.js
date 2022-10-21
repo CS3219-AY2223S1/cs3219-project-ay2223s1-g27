7f109aeceb6aee5d4e4b4d9044e99fd9e7fea133
Chi Sern <64767959+ngchisern@users.noreply.github.com>
2022-10-21 10:03:25 +0800

    Replace code editor (#91)
    * Install react-codemirror
    
    * Add simple editor
    
    * Update coding event resp format
    
    * Fix redundant update
    
    * Tidy
    
    * Remove monaco editor
    
    * Remove monaco dependencies
    
    * Update user service replica
    
    * Add code mirror themes
    
    * Fix themes
    
    * Add language extension
    
    * Tidy
    
    * Deploy
    
    * Deploy
    
    * revert

--- frontend/src/components/editor/ThemeDropdown.js
+++ frontend/src/components/editor/ThemeDropdown.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Select from "react-select";
-import monacoThemes from "monaco-themes/themes/themelist";
+import { codeMirrorThemes } from "../../lib/defineTheme";
 import { customStyles } from "../../constants/customStyles";
 import { Box, FormHelperText } from "@mui/material";
 
 const ThemeDropdown = ({ handleThemeChange, theme }) => {
   return (
     <Box display={"flex"} flexDirection={"column"}>
-    <FormHelperText>Editor Theme</FormHelperText>
-    <Select
-      placeholder={`Select Theme`}
-      // options={languageOptions}
-      options={Object.entries(monacoThemes).map(([themeId, themeName]) => ({
-        label: themeName,
-        value: themeId,
-        key: themeId,
-      }))}
-      value={theme}
-      styles={customStyles}
-      onChange={handleThemeChange} /></Box>
+      <FormHelperText>Editor Theme</FormHelperText>
+      <Select
+        placeholder={`Select Theme`}
+        // options={languageOptions}
+        options={Object.entries(codeMirrorThemes).map(([themeId, themeName]) => ({
+          label: themeName,
+          value: themeId,
+          key: themeId,
+        }))}
+        value={theme}
+        styles={customStyles}
+        onChange={handleThemeChange} /></Box>
   );
 };
 
