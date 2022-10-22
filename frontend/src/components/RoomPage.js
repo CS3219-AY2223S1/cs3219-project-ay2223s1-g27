77a4a194bef43a34049dafa22b7e3eb4841b0a04
Ng Zhen Teng <ngzhenteng@gmail.com>
2022-10-23 02:02:59 +0800

    Move chatSocket listeners into RoomPage

--- frontend/src/components/RoomPage.js
+++ frontend/src/components/RoomPage.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button, 
-  // Popover,
-  // Fab,
+  Popover,
+  Fab,
   Typography
 } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -13,33 +13,41 @@ import CodeEditorLanding from "./editor/CodeEditorLanding";
 import NavigationBar from "./NavigationBar";
 import ChatWindow from "./chat/ChatWindow";
 import { PREFIX_COLLAB_SVC, URL_COLLAB_SVC, URL_COMM_SVC, PREFIX_COMM_SVC_CHAT } from "../configs";
+import { INTERVIEWER_SWITCH_EVENT } from "../constants";
 import { jwtDecode } from "../util/auth";
 import LogoutIcon from '@mui/icons-material/Logout'; 
- 
+
 // import ResizePanel from "react-resize-panel";
-// import ChatIcon from '@mui/icons-material/Chat';
+import ChatIcon from '@mui/icons-material/Chat';
+import { isUnauthorizedError } from '@thream/socketio-jwt/build/UnauthorizedError.js'
+import { URL_USER_SVC_MESSAGE } from "../configs";
+import axiosApiInstance from "../axiosApiInstance";
 
 function RoomPage() {
   const [cookies] = useCookies();
   const location = useLocation(); // Location contains username and selected difficulty level
   const navigate = useNavigate();
-  // const [anchorEl, setAnchorEl] = useState(null);
+  // ChatWindow Props
+  const [isInterviewer, setIsInterviewer] = useState(); 
+  const [messages, setMessages] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
   // const [paneOpen, setPaneOpen] = useState(false);
-  // const handleClick = (event) => {
-  //   setAnchorEl(event.currentTarget);
-  // }
+  // console.log(location.state.difficultyLevel)
+  const room_id = location.state.room_id;
+  const username = jwtDecode(cookies['refresh_token']).username;
+  const chatWindowOpen = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
 
   // const handleOpen = () => {
   //   setPaneOpen(true);
   // }
 
-  // const handleClose = () => {
-  //   setAnchorEl(null);
-  // }
-
-  console.log(location.state.difficultyLevel)
-  const room_id = location.state.room_id;
-  const username = jwtDecode(cookies['refresh_token']).username;
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
 
   const codeEditorSocket = io(URL_COLLAB_SVC, {
     transports: ['websocket'],
@@ -58,7 +66,7 @@ function RoomPage() {
   });
 
   useEffect(() => {
-    console.log(location.state.difficultyLevel)
+    // console.log(location.state.difficultyLevel)
     codeEditorSocket.io.on("reconnection_attempt", () => {
       console.log('reconnection attempt')
     });
@@ -90,29 +98,66 @@ function RoomPage() {
         console.log("Other user has left")
     })
 
+    chatSocket.on(INTERVIEWER_SWITCH_EVENT, (data) => {
+      const interviewer = data.interviewer; 
+      console.log("Logging interviewer from ChatBody!")
+      console.log(data)
+      if (interviewer === username) {
+          setIsInterviewer(true); 
+      } else {
+          setIsInterviewer(false);
+      } 
+    })
+  
+    chatSocket.on('message response', (data) => {
+      console.log(data)
+      setMessages([...messages, data])
+      if (location.state.is_live) {
+      axiosApiInstance.post(URL_USER_SVC_MESSAGE, {room_id: room_id, messages: messages})
+      }
+    });
+  
+    chatSocket.on("connect_error", (err) => {
+      if (isUnauthorizedError(err)) {
+          console.log('User token has expired')
+      }
+      console.log(`connect_error due to ${err.message}`);
+    });
+
     return () => { // component will unmount equivalent
         chatSocket.emit('leave room', { room_id: room_id, username: username });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // Attempt history related useEffect
+  useEffect(() => {
+    console.log(messages)
+    // if (!location.state.is_live) {
+    //   axiosApiInstance.get(URL_USER_SVC_MESSAGE, {params: {room_id: room_id}}).then(x => {
+    //     if (x.data) {
+    //       setMessages(x.data.messages);
+    //     }
+    //   })
+    // }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const handleLeaveSession = () => {
     navigate("/landing", { state: { user: location.state.user } });
   }
-  // const open = Boolean(anchorEl);
-  // const id = open ? 'simple-popover' : undefined;
   return (
     <>
       <NavigationBar isAuthenticated={true} user={location.state.user} />
       <div> 
         <Typography sx={{ marginLeft: "3%", marginTop: "1%", marginBottom: "-2%" }} variant={"h3"} marginBottom={"2rem"}>Coding Room</Typography>
         {/* Uncomment this for popover chat */}
-        {/* <Fab style={{ left: 1800}} onClick={handleClick}>
+        <Fab style={{ left: 1200}} onClick={(e) => handleClick(e)}>
           <ChatIcon/>
         </Fab>          
         <Popover
-          id={id}
-          open={open}
+          id={'simple-popover'}
+          open={chatWindowOpen}
           anchorEl={anchorEl}
           onClose={handleClose}
           PaperProps={{
@@ -123,8 +168,8 @@ function RoomPage() {
             horizontal:'left'
           }}
         >
-          <ChatWindow chatSocket={chatSocket} room_id={room_id} username={username} /> 
-        </Popover> */}
+          <ChatWindow chatSocket={chatSocket} room_id={room_id} username={username} isInterviewer={isInterviewer} messages={messages}/> 
+        </Popover>
 
         {/* <Fab style={{ left: 1800}} onClick={handleOpen}>
           <ChatIcon/>
@@ -134,13 +179,13 @@ function RoomPage() {
           title="Messenger"
           onRequestClose={() => setPaneOpen(false)}
         > 
-          <ChatWindow chatSocket={chatSocket} room_id={room_id} username={username} />
+          <ChatWindow chatSocket={chatSocket} room_id={room_id} username={username} isInterviewer={isInterviewer} messages={messages} />
         </SlidingPane> */}
         <Box display={"flex"} flexDirection={"column"} style={{ marginTop: '3%', marginLeft: "3%", marginRight: "3%" }}>  
           <CodeEditorLanding socket={codeEditorSocket} chatSocket={chatSocket} room_id={room_id} username={username} cache={location.state.cache} is_live={location.state.is_live} />
           <div style={{ marginTop: '1%' }}></div>
           <h1 style={{ fontSize: '1.25rem', fontWeight: 'bold', marginBottom: '5px', color: '#0f172a' }}> Messenger </h1>
-          <ChatWindow chatSocket={chatSocket} room_id={room_id} username={username} is_live={location.state.is_live} />
+          {/* <ChatWindow chatSocket={chatSocket} room_id={room_id} username={username} isInterviewer={isInterviewer} messages={messages} /> */}
         </Box>  
         <Box display={"flex"} flexDirection={"row"} justifyContent={"flex-end"} sx={{ marginRight: "3%", marginBottom: "10px" }}>
           <Button
--- frontend/src/components/chat/ChatBody.js
+++ frontend/src/components/chat/ChatBody.js
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import QuestionSelector from "./QuestionSelector"; 
 import { Button, Box, Chip } from '@mui/material';
-import { INTERVIEWER_SWITCH_EVENT, INTERVIEWER_SWITCH_REQUEST_EVENT } from "../../constants";
-import { URL_USER_SVC_MESSAGE } from "../../configs";
-import axiosApiInstance from "../../axiosApiInstance";
+import { INTERVIEWER_SWITCH_REQUEST_EVENT } from "../../constants";
 
-const ChatBody = ({ chatSocket, username, room_id, is_live }) => {
-  const [isInterviewer, setIsInterviewer] = useState(); 
-  const [messages, setMessages] = useState([]);
-  
-  chatSocket.on(INTERVIEWER_SWITCH_EVENT, (data) => {
-    const interviewer = data.interviewer; 
-    console.log(data)
-    if (interviewer === username) {
-        setIsInterviewer(true); 
-    } else {
-        setIsInterviewer(false);
-    } 
-  })
- 
-  chatSocket.on('message response', (data) => {
-    console.log(data)
-    setMessages([...messages, data])
-    if (is_live) {
-      axiosApiInstance.post(URL_USER_SVC_MESSAGE, {room_id: room_id, messages: messages})
-    }
-  });
-
-  useEffect(() => {
-    if (!is_live) {
-      axiosApiInstance.get(URL_USER_SVC_MESSAGE, {params: {room_id: room_id}}).then(x => {
-        if (x.data) {
-          setMessages(x.data.messages);
-        }
-      })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+const ChatBody = ({ chatSocket, username, room_id, isInterviewer, messages }) => {
 
   const handleSwitchRole = () => {
+    console.log("Hellosssssssssss")
+    console.log(isInterviewer)
+
     if (!isInterviewer) {
       chatSocket.emit(INTERVIEWER_SWITCH_REQUEST_EVENT, {
         room_id: room_id,
@@ -92,11 +62,6 @@ const ChatBody = ({ chatSocket, username, room_id, is_live }) => {
           </div>
         </div>
         )}        
-
-        {/* This is triggered when a user is typing
-        <div className="message_status">
-          <p>Someone is typing...</p>
-        </div> */}
       </div>
     </>
   );
